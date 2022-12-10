b878adcd209753825063503182194693441b8c99
damazi1 <114773869+damazi1@users.noreply.github.com>
2022-12-10 20:03:50 +0100

    pierwszy_dzień
    zadanie z dnia 1

---
+++ zad1/zad1.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::ifstream input("./input.txt");
+
+	std::string line;
+	int current = 0;
+	int highest = 0;
+	while(std::getline(input, line))
+	{
+		if(line.length() == 0)
+		{
+			if(current > highest)
+			{
+				highest = current;
+			}
+			current = 0;
+		}
+		current += std::atoi(line.c_str());
+	}
+	input.close();
+	if(current > highest)
+	{
+		highest = current;
+	}
+
+	std::cout << "Total " << highest << " calories that elf carrying\n";
+
+	return EXIT_SUCCESS;
+}
