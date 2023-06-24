0a248bcd94a7bec155d8f58a09cc63b7edfad1c9
damazi1 <114773869+damazi1@users.noreply.github.com>
2023-06-24 18:57:58 +0200

    test

---
+++ zad1/test.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int *ile(ifstream &in, int n)
+{
+    int *t = new int[n];
+    int suma = 0, i = 0;
+    string s;
+    while (getline(in, s))
+    {
+        if (s.length() == 0)
+        {
+            cout << s a'<< endl;
+            t[i] = suma;
+            i++;
+            suma = 0;
+        }
+        else
+        {
+            cout << s << endl;
+            suma += stoi(s);
+            cout << suma << endl;
+        }
+    }
+    return t;
+}
+
+int main()
+{
+    ifstream in("input.txt");
+    int *t = ile(in, 300);
+    if (t)
+    {
+        delete[] t;
+        t = nullptr;
+    }
+
+    in.close();
+    return 0;
+}
